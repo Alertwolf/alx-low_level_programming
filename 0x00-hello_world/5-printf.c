fc888201afd20d00d664024b5d4a1d437e5712f9
Alertwolf <yassr22yakub@gmail.com>
2023-02-16 15:29:45 -0800

    Project 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#inlcude <stdio.h>
+/**
+ * main - a programme that print  line that is using the printf funcion 
+ * int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
