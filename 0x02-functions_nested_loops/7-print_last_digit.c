c2ba03288baac2ef9f5267cea5a0b6b90dc8749e
Alertwolf <yassr22yakub@gmail.com>
2023-02-22 13:54:45 -0800

    Project 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,15 @@
 /**
  * print_last_digit - function that prints last digit
  *
- * @i: function parameter
+ * @k: function parameter
  *
  * Return: k
  */
 
-int print_last_digit(int i)
+int print_last_digit(int k)
 {
-	int i;
+	int k;
 
-	k = i % 10;
 	if (i < 0)
 		k = -k;
 	_putchar(k + '0');
