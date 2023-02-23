1cba9e4a42b093f3fb237bcda31f650ad3090ad6
Alertwolf <yassr22yakub@gmail.com>
2023-02-22 17:13:00 -0800

    Project 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - function to print natural numbers
  *
- *@n: function parameter
+ * @n: function parameter
  *
- *Return: always 0
- */
+ * Return: always 0
+ */ 
 
 void print_to_98(int n)
 {
